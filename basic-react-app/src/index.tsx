185bc3a4ad187849798cb401fa6c2f66cc18b268
Maximilian Bossert <max.bossert@outlook.de>
2021-07-14 18:05:17 +0200

    remove package for styling

--- basic-react-app/src/index.tsx
+++ basic-react-app/src/index.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as ABB from "@abb/abb-common-ux-react";
-
-import "@abb/abb-common-ux-react/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ABB.AppContainer theme={"light"} style={{ width: "100%", height: "98vh" }}>
-      <ABB.AbbBar productName="Basic React App" />
-      <ABB.AppContent style={{ width: "100%" }}>
-        <ABB.AppMainContent>
-          <p>Hello World</p>
-        </ABB.AppMainContent>
-      </ABB.AppContent>
-      <ABB.AppFooter showCopyright={true} showLogo={false} />
-    </ABB.AppContainer>
+    <div>
+      <p>Hello World</p>
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
